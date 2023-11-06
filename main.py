de0be0d4f6fbc8e63187bb7a0912633096fbe416
z933 <142270708+z933@users.noreply.github.com>
2023-11-06 01:59:17 +0000

    Added operation choice
    V 6.11.2023.2

--- main.py
+++ main.py
@@ -15,3 +15,22 @@ def generate_question(operation):
 
 def validate_answer(user_answer, correct_answer):
   return int(user_answer) == correct_answer
+  
+def main(): 
+  print("Math Quiz")
+  print("Choose an operation:")
+  print("1. Addition (+)")
+  print("2. Subtraction (-)")
+  print("3. Multiplication (*)")
+  print("4. Division (/)")
+  operation_choice = input("Enter the number of the operation: ")
+
+  operations = {
+      "1": "+",
+      "2": "-",
+      "3": "*",
+      "4": "/",
+  }
+  if operation_choice not in operation:
+    print("Invalid choice. Please select a valid choice operation.")
+    return
