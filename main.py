76751cf205ed6e1c775adaf19685215dfce45743
z933 <142270708+z933@users.noreply.github.com>
2023-11-07 21:11:37 +0000

    Added comments to the program

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-# This program is made by Carl.
+# This program is made by Carl Bombales.
 import random
-
-def generate_question(operation):
-  num1 = random.randint(1,20)
-  num2 = random.randint(1,20)
+# Sequence One: Number Generator
+# Purpose: Generating a random integer between 1 and 20
+# Author: Carl
+# Date 7/11/2023
+# version: 6.11.23
+def generate_question(operation): 
+  num1 = random.randint(1,20) # Generates a random number from 1 to 20.  
+  num2 = random.randint(1,20) # Generates a random number from 1 to 20.
   if operation == "+":
-    return f"{num1} + {num2}", num1 + num2
+    return f"{num1} + {num2}", num1 + num2 # This function Generates a random Addition Question.
   elif operation == "-":
-    return f"{num1} - {num2}", num1 - num2
+    return f"{num1} - {num2}", num1 - num2 # This function Generates a random Subtraction Question.
   elif operation == "*":
-    return f"{num1} * {num2}", num1 * num2
+    return f"{num1} * {num2}", num1 * num2 # This function Generates a random Multiplication Question.
   elif operation == "/":
-    return f"{num1} / {num2}", num1 / num2
+    return f"{num1} / {num2}", num1 / num2 # This function Generates a random Division Question.
 
 def validate_answer(user_answer, correct_answer):
-  return int(user_answer) == correct_answer
+  return int(user_answer) == correct_answer # The variable correct_answer has all the answer from each operation.
+# Sequence Two: Operation choice
+# Purpose: Asks the user which operation would they like to answer
+# Author: Carl Bombales 
+# Data:
   
 def main(): 
   print("Math Quiz")
